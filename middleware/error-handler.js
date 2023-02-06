14801e55e46d143adb9ec645d84e9103137de58b
Alan <prossliu@gmail.com>
2023-02-06 20:10:45 +1030

    init project

---
+++ middleware/error-handler.js
@@ -0,0 +1,32 @@
+const { StatusCodes } = require('http-status-codes');
+
+const errorHandlerMiddleware = (err, req, res) => {
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later',
+  };
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+    // eslint-disable-next-line no-param-reassign
+    err.statusCode = 400;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
---
+++ utils/swagger.js
@@ -0,0 +1,17 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+module.exports = swaggerJsDoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Apis Documentation',
+      version: '1.0.0',
+      contact: {
+        name: 'John',
+        email: 'jzeva1213@gmail.com',
+      },
+      description: 'This is the API documentation',
+    },
+  },
+  apis: ['controllers/*.js'],
+});
