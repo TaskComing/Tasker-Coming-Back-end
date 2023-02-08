b9c321b570d2101e5358f4d3c43dfbc1f01f3651
Alan <prossliu@gmail.com>
2023-02-09 01:03:23 +1030

    change default port

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc));
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
