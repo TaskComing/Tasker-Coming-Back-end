63c2933b3d286fb6325f587461d779467647c3fd
Alan <prossliu@gmail.com>
2023-03-14 19:31:11 +1030

    fix

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const auth = require('../middleware/authentication');
 
 const v1Router = express.Router();
 
-v1Router.use('/auth', auth, authRouter);
+v1Router.use('/auth', authRouter);
 v1Router.use('/products', productRouter);
 
 v1Router.use('/tasks', taskRouter);
