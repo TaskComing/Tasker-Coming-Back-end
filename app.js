12d776d6876da390b3dbcd1c3604bccc0627974f
Jeremy Yi <yiyanhong10233@gmail.com>
2023-03-18 14:10:57 +1100

    save

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./config/passport')(passport);
 const swaggerUi = require('swagger-ui-express');
 const { connectDB } = require('./db/connect');
 const notFoundMiddleware = require('./middleware/not-found');
@@ -17,8 +21,8 @@ const app = express();
 app.use(express.json());
 // extra security package
 app.use(helmet());
-app.use(cors());
 app.use(xss());
+app.use(cors());
 // routes
 app.use('/v1', v1Router);
 
@@ -27,6 +31,17 @@ app.get('/healthcheck', (request, response) => response.status(200).send({ messa
 
 // swagger api docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc));
+/**
+ * google auth
+ */
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: ['J5IAoT04te'],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // error handler
 app.use(notFoundMiddleware);
@@ -36,7 +51,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
     app.listen(port, () => console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const auth = require('../middleware/authentication');
 
 const router = express.Router();
@@ -6,5 +7,27 @@ const { registerUser, loginUser, forgotPassword, resetPassword } = require('../c
 
 router.post('/register', registerUser);
 router.post('/login', auth, loginUser);
+
+// v1/auth/google
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+// v1/auth/google/callback
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+  // Redirect the user to the homepage or another route after successful authentication
+  res.redirect('/');
+});
+// v1/auth/logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/current_user', (req, res) => {
+  res.send(req.user);
+});
 
 module.exports = router;
