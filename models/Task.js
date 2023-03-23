0ef4fce583f68e32e1df3e0599a8bbe1b475b2a6
GUANJIEZHONG <samzhongguanjie@gmail.com>
2023-03-23 16:55:18 +1000

    update

--- models/Task.js
+++ models/Task.js
@@ -8,7 +8,7 @@ const schema = new mongoose.Schema({
   },
   title: {
     type: String,
-    required: true,
+    // required: true,
   },
   state: {
     type: Number,
@@ -17,56 +17,56 @@ const schema = new mongoose.Schema({
     default: false,
     type: Boolean,
   },
-  due_time: {
-    type: Date,
-    default: Date.now,
-  },
-  remote: {
-    type: Boolean,
-  },
-  x: {
-    type: mongoose.Decimal128,
-    default: 0,
-  },
-  y: {
-    type: mongoose.Decimal128,
-    default: 0,
-  },
-  detail: {
-    type: String,
-  },
-  images: {
-    type: [String],
-  },
-  budget: {
-    type: Number,
-  },
-  type: {
-    type: String,
-  },
-  tag: {
-    type: String,
-  },
-  status: {
-    type: String,
-    default: 'open',
-  },
-  final_price: {
-    type: Number,
-  },
-  create_user_id: {
-    type: Number,
-  },
-  create_datetime: {
-    //数据库的名字是datetime
-    type: Date,
-  },
-  comments: {
-    type: [String],
-  },
-  offers: {
-    type: Array,
-  },
+  // due_time: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+  // remote: {
+  //   type: Boolean,
+  // },
+  // x: {
+  //   type: mongoose.Decimal128,
+  //   default: 0,
+  // },
+  // y: {
+  //   type: mongoose.Decimal128,
+  //   default: 0,
+  // },
+  // detail: {
+  //   type: String,
+  // },
+  // images: {
+  //   type: [String],
+  // },
+  // budget: {
+  //   type: Number,
+  // },
+  // type: {
+  //   type: String,
+  // },
+  // tag: {
+  //   type: String,
+  // },
+  // status: {
+  //   type: String,
+  //   default: 'open',
+  // },
+  // final_price: {
+  //   type: Number,
+  // },
+  // create_user_id: {
+  //   type: Number,
+  // },
+  // create_datetime: {
+  //   //数据库的名字是datetime
+  //   type: Date,
+  // },
+  // comments: {
+  //   type: [String],
+  // },
+  // offers: {
+  //   type: Array,
+  // },
 });
 
 const TaskModel = mongoose.model('Task', schema);
