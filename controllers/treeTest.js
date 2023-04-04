c86dbd7f24f15ca1e236d2fd42d6eb2f226ca3ea
G <ygy3389@gmail.com>
2023-04-04 11:42:23 +1000

    flat structure

--- controllers/treeTest.js
+++ controllers/treeTest.js
@@ -7,19 +7,41 @@ const addTreeTest = async (req, res) => {
   res.status(201).json(treeTest);
 };
 
+// const getTree = async (Model, id) => {
+//   const currentNode = await Model.findById(id).exec();
+//   const { childIdArray } = currentNode;
+//   if (childIdArray && childIdArray.length === 0) {
+//     return { ...currentNode._doc, childNode: [] };
+//   }
+//   if (!childIdArray) {
+//     return { ...currentNode._doc, childNode: [] };
+//   }
+//   // eslint-disable-next-line no-return-await
+//   const childPromises = childIdArray.map(async (child) => await getTree(Model, child));
+//   const childNodes = await Promise.all(childPromises);
+//   return { ...currentNode._doc, childNode: childNodes };
+// };
+
 const getTree = async (Model, id) => {
   const currentNode = await Model.findById(id).exec();
   const { childIdArray } = currentNode;
+
   if (childIdArray && childIdArray.length === 0) {
-    return { ...currentNode._doc, childNode: [] };
+    return [currentNode];
   }
+
   if (!childIdArray) {
-    return { ...currentNode._doc, childNode: [] };
+    return [currentNode];
   }
-  // eslint-disable-next-line no-return-await
-  const childPromises = childIdArray.map(async (child) => await getTree(Model, child));
+
+  const childPromises = childIdArray.map(async (child) => {
+    const childNodes = await getTree(Model, child);
+    return childNodes;
+  });
+
   const childNodes = await Promise.all(childPromises);
-  return { ...currentNode._doc, childNode: childNodes };
+
+  return [currentNode, ...childNodes.flat()];
 };
 
 const getTreeById = async (req, res) => {
