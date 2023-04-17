894871e8802994eb27c4a637da0db8bc2fac09e0
L80W1998 <L80W1998@gmail.com>
2023-04-17 21:56:40 +1000

    TAS-54-improved 17/04

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,26 +3,40 @@ const CommentModel = require('../models/Comment');
 const UserModel = require('../models/User');
 
 const addComment = async (req, res) => {
-  const { text, create_datetime, create_user_id } = req.body;
-  const comment = new CommentModel({ text, create_datetime, create_user_id });
+  const { text, create_datetime, create_user_id, task_id } = req.body;
+  const comment = new CommentModel({ text, create_datetime, create_user_id, task_id });
   await comment.save();
   res.status(201).json(comment);
 };
 
 const getAllComments = async (req, res) => {
-  const comments = await CommentModel.find().populate('create_user_id').exec();
-  res.status(200).json(comments);
+  const comments = await CommentModel.find()
+    .populate('replied_comment_ids')
+    .populate('create_user_id')
+    .exec();
+  const allQuestions = comments.filter((comment) => !comment.parent_comment_id);
+  res.status(200).json(allQuestions);
 };
 
-const getCommentById = async (req, res) => {
+const getAllCommentsByTask = async (req, res) => {
   const { id } = req.params;
-  const comment = await CommentModel.findById(id).populate('create_user_id').exec();
-  if (!comment) {
-    res.status(404).json({ error: 'comment not found' });
-    return;
-  }
-  res.json(comment);
+  const comments = await CommentModel.findById(id)
+    .populate('replied_comment_ids')
+    .populate('create_user_id')
+    .exec();
+  const allQuestions = comments.filter((comment) => !comment.parent_comment_id);
+  res.status(200).json(allQuestions);
 };
+
+// const getCommentById = async (req, res) => {
+//   const { id } = req.params;
+//   const comment = await CommentModel.findById(id).populate('create_user_id').exec();
+//   if (!comment) {
+//     res.status(404).json({ error: 'comment not found' });
+//     return;
+//   }
+//   res.json(comment);
+// };
 
 const updateCommentById = async (req, res) => {
   const { id } = req.params;
@@ -62,7 +76,8 @@ const deleteCommentById = async (req, res) => {
 const addReplyToComment = async (req, res) => {
   const { text, create_datetime } = req.body;
   // const parentCommentId = req.params.id;
-  const parent_comment_id = req.query.id;
+  const { id: parent_comment_id } = req.params;
+  // const parent_comment_id = req.query.id;
   const reply = new CommentModel({ text, parent_comment_id, create_datetime });
   await reply.save();
   const replied_comment_id = reply._id;
@@ -92,6 +107,24 @@ const addCommentToUser = async (req, res) => {
   res.json(updateUser);
 };
 
+const addCommentToTask = async (req, res) => {
+  const { taskId, commentId } = req.params;
+  const task = await UserModel.findById(taskId).exec();
+  const comment = await CommentModel.findById(commentId).exec();
+  if (!task || !comment) {
+    res.status(404).json({ error: 'task or comment not found' });
+    return;
+  }
+  comment.task_id = task._id;
+  await comment.save();
+  const updateTask = await UserModel.findByIdAndUpdate(
+    task._id,
+    { $addToSet: { comments: comment._id } },
+    { new: true }
+  );
+  res.json(updateTask);
+};
+
 // const removeCommentFromUser = async (req, res) => {
 //   const { userId, commentId } = req.params;
 //   const user = await UserModel.findById(userId).exec();
@@ -113,10 +146,12 @@ const addCommentToUser = async (req, res) => {
 module.exports = {
   addComment,
   getAllComments,
-  getCommentById,
+  getAllCommentsByTask,
+  // getCommentById,
   updateCommentById,
   deleteCommentById,
   addReplyToComment,
   addCommentToUser,
+  addCommentToTask,
   // removeCommentFromUser,
 };
--- routes/comments.js
+++ routes/comments.js
@@ -3,12 +3,14 @@ const auth = require('../middleware/authentication');
 
 const {
   addComment,
+  getAllCommentsByTask,
   getAllComments,
-  getCommentById,
+  // getCommentById,
   updateCommentById,
   deleteCommentById,
-  // addReplyToComment,
+  addReplyToComment,
   addCommentToUser,
+  addCommentToTask,
   // removeCommentFromUser,
 } = require('../controllers/comments');
 
@@ -16,12 +18,12 @@ const commentRouter = Router();
 
 commentRouter.post('', auth, addComment);
 commentRouter.get('', getAllComments);
-commentRouter.get('/:id', auth, getCommentById);
+commentRouter.get('/:id', getAllCommentsByTask);
 commentRouter.put('/:id', auth, updateCommentById);
 commentRouter.delete('/:id', auth, deleteCommentById);
 // commentRouter.post('/:id', addReplyToComment);
-// commentRouter.post('/:commentId/replies/:replyId', auth, addReplyToComment);
-
+commentRouter.post('/:id', auth, addReplyToComment);
+commentRouter.post('/:commentId/tasks/:taskId', auth, addCommentToTask);
 commentRouter.post('/:commentId/users/:userId', auth, addCommentToUser);
 // commentRouter.delete('/:commentId/users/:userId', removeCommentFromUser);
 
